467c278f83ec63feae2a27ad49f2fdd45aa563ca
Dallin Stirling <dallinstirling@gmail.com>
2022-08-09 23:39:39 -0600

    Add project files.

---
+++ FinalSynthesis/Pages/ThingsToRemember.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalSynthesis.Pages
+{
+    public class ThingsToRememberModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ FinalSynthesis/Pages/WhatILearned.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalSynthesis.Pages
+{
+    public class WhatILearnedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
